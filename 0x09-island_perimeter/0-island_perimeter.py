6c52572ec8008d642bc0a20c336a2a2661a7c004
LouisCR04 <louiskariuki04@gmail.com>
2024-08-01 22:07:25 +0300

    Bug fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,14 +13,15 @@ def island_perimeter(grid):
         """ Recursive solution to finding perimeter of each cell """
         if (m < 0 or n < 0 or m >= rows or n >= cols or grid[m][n] == 0):
             return 1
-        if [m, n] in visited:
+        if (m, n) in visited:
             return 0
 
-        visited.add([m, n])
+        visited.add((m, n))
         perimeter = dfs(m + 1, n) + dfs(m - 1, n) + dfs(m, n + 1) + dfs(m, n - 1)
 
         return perimeter
 
     for row in range(rows):
         for col in range(cols):
-            return dfs(row, col)
+            if grid[row][col]:
+                return dfs(row, col)
